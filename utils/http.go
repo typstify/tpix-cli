9cff30a0705d6106e3bf8010d92ffb172f46590c
zjzhang <zhangzj33@gmail.com>
2026-04-22 11:54:33 +0800

    Fix OpenUrl on Windows

--- utils/http.go
+++ utils/http.go
@@ -16,7 +16,7 @@ func OpenURL(url string) error {
 	switch runtime.GOOS {
 	case "windows":
 		cmd = "cmd"
-		args = []string{"/c", "start"}
+		args = []string{"/c", "start", "", url}
 	case "darwin":
 		cmd = "open"
 		args = []string{url}
@@ -25,17 +25,14 @@ func OpenURL(url string) error {
 		if isWSL() {
 			// Use 'cmd.exe /c start' to open the URL in the default Windows browser
 			cmd = "cmd.exe"
-			args = []string{"/c", "start", url}
+			args = []string{"/c", "start", "", url}
 		} else {
 			// Use xdg-open on native Linux environments
 			cmd = "xdg-open"
 			args = []string{url}
 		}
 	}
-	if len(args) > 1 {
-		// args[0] is used for 'start' command argument, to prevent issues with URLs starting with a quote
-		args = append(args[:1], append([]string{""}, args[1:]...)...)
-	}
+
 	return exec.Command(cmd, args...).Start()
 }
 
